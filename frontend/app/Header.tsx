7d76094355a2f8bc42e04451ce6425b964faa000
roshray <roshanroy759@gmail.com>
2022-12-14 20:08:06 +0530

    Navbar

--- frontend/app/Header.tsx
+++ frontend/app/Header.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import {Bars3Icon} from "@heroicons/react/24/solid"
+import Link from "next/link"
+import NavLinks from "./NavLinks"
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <div className="grid grid-cols-3 p-10 items-center">
+            <Bars3Icon className="h-8 w-8 cursor-pointer"/>
+            <Link href="/" prefetch={false}>
+                <h1 className="font-serif text-4xl text-center">
+                    The Whistleblower
+                </h1>
+            </Link>
+
+            <div className="flex items-center justify-end space-x-2">
+                {/* Dark Mode Button */}
+
+                <button className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8  py-2 lg:py-4 rounded-full dark:bg-slate-800"> 
+                    Subscribe Now
+                </button>
+            </div>
+        </div>
+        {/* NavLinks */}
+
+        <NavLinks />
+
+
+
+
+
+        {/* Search */}
+    </header>
   )
 }
 
