2f49c7545acff68d736fbe4fdc022b8047856082
roshray <roshanroy759@gmail.com>
2022-12-18 19:57:54 +0530

    demo

--- frontend/app/Article.tsx
+++ frontend/app/Article.tsx
@@ -24,7 +24,9 @@ function Article({article}: Props) {
 
                 <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
                     <p>{article.source} -</p>
-                    <p>{article.published_at}</p>
+                    <p>
+                        {article.published_at}
+                    </p>
                 </footer>
             </div>
             {/* ReadMorebutton */}
---
+++ frontend/app/LiveTimestamp.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import Timeago from 'react-timeago'
+
+type Props = {
+    time: string
+}
+function LiveTimestamp({time}: Props) {
+  return (
+    <div>LiveTimestamp</div>
+  )
+}
+
+export default LiveTimestamp
